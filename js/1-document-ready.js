6ad806b0665b6551d1626e5d8aaa11494d1ad324
monika-and <monica.andrzejewska@gmail.com>
2017-06-24 14:29:33 +0200

    selektory

---
+++ js/1-document-ready.js
@@ -0,0 +1,5 @@
+'use strict';
+//poniższa funkcja wywołą sie tylko po załadowaniu strony - odpowiednia składnia funkcji
+$(function() {
+  $('#first').css({color: "red"}).hide(2000).show(3000);
+  });
---
+++ js/2-selektory.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//pobieranie elementów po znaczniku
+var paragraphs = $('p');
+console.log(paragraphs);
+
+//pobieranie elementów po id
+var paragraphOne = $('#paragraf');
+console.log(paragraphOne);
+
+//pobieranie elementów po klasie
+var paragraphTwo = $('.paragraf');
+console.log(paragraphTwo);
+
